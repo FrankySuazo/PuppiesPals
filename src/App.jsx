dd2b040e72a77c5cf968f7de2e42355468868927
FrankySuazo <frankysuazo@outlook.com>
2024-01-26 00:36:15 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { puppyList } from "./data.js";
+
+export default function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick(id) {
+    setFeatPupId(id);
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  return (
+    <div>
+      {puppies.map((puppy) => (
+        <p
+          key={puppy.id}
+          onClick={() => {
+            handleClick(puppy.id);
+          }}
+        >
+          {puppy.name}
+        </p>
+      ))}
+
+      {featuredPup && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
